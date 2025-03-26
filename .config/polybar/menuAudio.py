51b3e65a8e19f58dc2d1ef0074cb4024c1726e01
Tenegan <tenegan.billot@orange.fr>
2025-03-26 13:19:36 +0100

    Add audio output selection in polybar

---
+++ .config/polybar/menuAudio.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+import sys
+import subprocess
+import re
+
+def get_info() -> str:
+    sink = subprocess.run(
+        ["pactl", "list", "sinks"],
+        capture_output = True, # Python >= 3.7 only
+        text = True # Python >= 3.7 only
+    ).stdout
+    
+    return sink
+
+
+def proc_devices(sinks: str) -> list:
+
+# Sink #(?P<id>[0-9])+$\n\tState: (?P<state>.*)\n\tName:(?P<name>.*)$\n\tDescription: (?P<description>.*)$\n.*\n.*\n.*\n.*\n\tMute: (?P<muted>(?:yes)|(?:no))$
+    pattern = r"Sink #(?P<id>[0-9])+$\n\tState: (?P<state>.*)\n\tName: (?P<name>.*)$\n\tDescription: (?P<description>.*)$\n.*\n.*\n.*\n.*\n\tMute: (?P<muted>(?:yes)|(?:no))$"
+    
+    matches = re.findall(pattern, sinks, re.MULTILINE)
+
+    return matches
+
+
+def print_choice(choices: list):
+    
+    for match in choices:
+        output = f"{match[3]}"
+
+        # if match[4] == "yes":
+         #   output += " Muted"
+
+        print(output)
+
+    print("Quit")
+
+
+def main():
+    sinks = get_info()
+
+    devices = proc_devices(sinks)
+    
+    arg = sys.argv[-1]
+
+    if arg == "Quit":
+        return
+    
+    for device in devices:
+        if arg.find(device[3]) >= 0:
+            output = subprocess.run(
+                ["pactl", "set-default-sink", device[2]],
+                capture_output = True, # Python >= 3.7 only
+                text = True # Python >= 3.7 only
+            )
+            return
+
+    print_choice(devices)
+
+
+if __name__ == "__main__":
+    main()
